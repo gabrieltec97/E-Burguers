3c6e697c3f74aed3fbe3ca6c9d4a274099906703
Gabriel Silva <47263072+gabrieltec97@users.noreply.github.com>
2021-06-29 17:14:18 -0300

    Ajustes em autenticação

--- routes/web.php
+++ routes/web.php
@@ -15,106 +15,106 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
-Route::resource('/pedidos', 'OrdersController');
+Route::resource('/pedidos', 'OrdersController')->middleware('auth');
 
-Route::resource('/clientes', 'ClientsController');
+Route::resource('/clientes', 'ClientsController')->middleware('auth');
 
-Route::resource('/usuario', 'UserController');
+Route::resource('/usuario', 'UserController')->middleware('auth');
 
-Route::resource('/preparo', 'PreparingController');
+Route::resource('/preparo', 'PreparingController')->middleware('auth');
 
-Route::resource('/cupons', 'CouponController');
+Route::resource('/cupons', 'CouponController')->middleware('auth');
 
-Route::resource('/refeicoes', 'menuController');
+Route::resource('/refeicoes', 'menuController')->middleware('auth');
 
-Route::resource('/minhaBandeja', 'TrayController');
+Route::resource('/minhaBandeja', 'TrayController')->middleware('auth');
 
-Route::resource('/itensAdicionais', 'ExtrasController');
+Route::resource('/itensAdicionais', 'ExtrasController')->middleware('auth');
 
 //Rotas de pedido.
 
-Route::get('/tipoPedido', 'TrayController@orderType')->name('tipoPedido');
+Route::get('/tipoPedido', 'TrayController@orderType')->name('tipoPedido')->middleware('auth');
 
 //Rotas de combo
 
-Route::get('/hamburguer', 'TrayController@orderComboHamburguer')->name('comboHamburguer');
+Route::get('/hamburguer', 'TrayController@orderComboHamburguer')->name('comboHamburguer')->middleware('auth');
 
-Route::get('/acompanhamento/{id}', 'TrayController@fries')->name('acompanhamento');
+Route::get('/acompanhamento/{id}', 'TrayController@fries')->name('acompanhamento')->middleware('auth');
 
-Route::post('/bebida/{id}', 'TrayController@drinks')->name('bebida');
+Route::post('/bebida/{id}', 'TrayController@drinks')->name('bebida')->middleware('auth');
 
-Route::post('/salvamento/{id?}', 'TrayController@shoppingFinish')->name('salvamento');
+Route::post('/salvamento/{id?}', 'TrayController@shoppingFinish')->name('salvamento')->middleware('auth');
 
-Route::get('/finalizarCompra', 'TrayController@reviewAndFinish')->name('fimCompra');
+Route::get('/finalizarCompra', 'TrayController@reviewAndFinish')->name('fimCompra')->middleware('auth');
 
-Route::post('/removerAdicional', 'TrayController@removeExtras')->name('removeadd');
+Route::post('/removerAdicional', 'TrayController@removeExtras')->name('removeadd')->middleware('auth');
 
 //Rotas de edição do combo.
 
-Route::get('/editarComboAcompanhamento', 'TrayController@editPortion')->name('editarPorcao');
+Route::get('/editarComboAcompanhamento', 'TrayController@editPortion')->name('editarPorcao')->middleware('auth');
 
-Route::get('/editarComboBebida', 'TrayController@editDrink')->name('editarBebida');
+Route::get('/editarComboBebida', 'TrayController@editDrink')->name('editarBebida')->middleware('auth');
 
-Route::get('/editarComboExtras/{id?}', 'TrayController@editComboExtras')->name('editarComboExtras');
+Route::get('/editarComboExtras/{id?}', 'TrayController@editComboExtras')->name('editarComboExtras')->middleware('auth');
 
 //Rotas de pedido avulso.
 
-Route::get('/cardapio/{insert?}', 'menuController@foodMenu')->name('cardapio');
+Route::get('/cardapio/{insert?}', 'menuController@foodMenu')->name('cardapio')->middleware('auth');
 
-Route::get('/adicionarItem/{id}', 'TrayController@freeOrder')->name('adicionarItem');
+Route::get('/adicionarItem/{id}', 'TrayController@freeOrder')->name('adicionarItem')->middleware('auth');
 
-Route::post('/removerItem/{id}', 'TrayController@detached')->name('removerItem');
+Route::post('/removerItem/{id}', 'TrayController@detached')->name('removerItem')->middleware('auth');
 
-Route::post('/removerPersonalizado/{id}', 'TrayController@removePersonalized')->name('removerPersonalizado');
+Route::post('/removerPersonalizado/{id}', 'TrayController@removePersonalized')->name('removerPersonalizado')->middleware('auth');
 
-Route::post('/editarPersonalizado/{id}', 'TrayController@editPersonalized')->name('editarPersonalizado');
+Route::post('/editarPersonalizado/{id}', 'TrayController@editPersonalized')->name('editarPersonalizado')->middleware('auth');
 
-Route::post('/removeCustomizado/{id}', 'TrayController@removeCustom')->name('removeCustomizado');
+Route::post('/removeCustomizado/{id}', 'TrayController@removeCustom')->name('removeCustomizado')->middleware('auth');
 
-Route::post('/adicionaExtraComum/{id}', 'TrayController@addExtraItem')->name('addExtraItem');
+Route::post('/adicionaExtraComum/{id}', 'TrayController@addExtraItem')->name('addExtraItem')->middleware('auth');
 
 //Rota de aplicação de cupom.
 
-Route::post('/aplicarCupom', 'TrayController@couponApply')->name('aplicarCupom');
+Route::post('/aplicarCupom', 'TrayController@couponApply')->name('aplicarCupom')->middleware('auth');
 
-Route::post('/removerCupom/{couponName}', 'TrayController@couponRemove')->name('removerCupom');
+Route::post('/removerCupom/{couponName}', 'TrayController@couponRemove')->name('removerCupom')->middleware('auth');
 
 
 //Rotas de alteração de status dos pedidos.
-Route::post('/alterarStatus/{id}/{acao}/{remetente}/{idCliente}', 'OrdersController@changeStatus')->name('alterarStatus');
+Route::post('/alterarStatus/{id}/{acao}/{remetente}/{idCliente}', 'OrdersController@changeStatus')->name('alterarStatus')->middleware('auth');
 
 
 //Rotas de gerenciamento.
-Route::get('/emPreparo', 'PreparingController@toPrepare')->name('emPreparo');
+Route::get('/emPreparo', 'PreparingController@toPrepare')->name('emPreparo')->middleware('auth')->middleware('auth');
 
-Route::get('/meusDados', 'ClientsController@myData')->name('meusDados');
+Route::get('/meusDados', 'ClientsController@myData')->name('meusDados')->middleware('auth')->middleware('auth');
 
-Route::get('/meusPedidos', 'OrdersController@clientsOrders')->name('meusPedidos');
+Route::get('/meusPedidos', 'OrdersController@clientsOrders')->name('meusPedidos')->middleware('auth')->middleware('auth');
 
-Route::get('/gerenciamento', 'UserController@management')->name('gerenciamento');
+Route::get('/gerenciamento', 'UserController@management')->name('gerenciamento')->middleware('auth')->middleware('auth');
 
-Route::get('/financeiro', 'FinancialController@index')->name('financeiro');
+Route::get('/financeiro', 'FinancialController@index')->name('financeiro')->middleware('auth');
 
-Route::get('/dashboard', 'FinancialController@dashboard')->name('dashboard');
+Route::get('/dashboard', 'FinancialController@dashboard')->name('dashboard')->middleware('auth');
 
-Route::get('/pedidosPendentes', 'OrdersController@pending')->name('pedidosPendentes');
+Route::get('/pedidosPendentes', 'OrdersController@pending')->name('pedidosPendentes')->middleware('auth');
 
-Route::get('/deletarPendente/{id}', 'OrdersController@deletePending')->name('deletaPendente');
+Route::get('/deletarPendente/{id}', 'OrdersController@deletePending')->name('deletaPendente')->middleware('auth');
 
-Route::get('/confirmarPendente/{id}', 'OrdersController@confirmPending')->name('confirmaPendente');
+Route::get('/confirmarPendente/{id}', 'OrdersController@confirmPending')->name('confirmaPendente')->middleware('auth');
 
-Route::get('/confirmarPedido', 'OrdersController@confirm')->name('confirmarPedido');
+Route::get('/confirmarPedido', 'OrdersController@confirm')->name('confirmarPedido')->middleware('auth')->middleware('auth')->middleware('auth');
 
 //Rotas do ACL
 Route::resource('/roles', 'RoleController');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 
-Route::get('/dados', 'HomeController@getData')->name('dados');
+Route::get('/dados', 'HomeController@getData')->name('dados')->middleware('auth');
 
 //Rotas Ajax
 
